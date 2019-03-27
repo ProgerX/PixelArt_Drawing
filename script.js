01728de6ef2fe72d48974b81a1b0b96f6b1def7b
ProgerX <vahepogosyan@gmail.com>
2019-03-28 00:13:35 +0500

    toolsBeta

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
+const pixelSize = 10;
 let Canvas = document.getElementById("Draw_Canvas");
 let ctx = Canvas.getContext("2d");
+let backCanvas = document.getElementById("Back_Canvas");
+let ctxBack = backCanvas.getContext("2d");
 let W = Canvas.offsetWidth;
 let H = Canvas.offsetHeight;
 let p2;
+let Color = document.getElementById("colorPicker").value;
+let toolIndex = 1;
+
+function colorChanged(){
+    Color = document.getElementById("colorPicker").value;
+}
+function toolChange(vlu){
+    toolIndex=vlu;   
+}
+
 drawGrid();
 function drawGrid(){
    
     let col = "rgb(217, 217, 217)";
 
-    for(let i = 0, k = true; i < W/10; i++){        
-        for(let j = 0; j < H/10; j++){
+    for(let i = 0, k = true; i < W/pixelSize; i++){        
+        for(let j = 0; j < H/pixelSize; j++){
             if(k){
                 col = "rgb(217, 217, 217)";
                 k=false;
@@ -19,8 +32,8 @@ function drawGrid(){
             k=true;
             }
 
-            ctx.fillStyle = col;
-            ctx.fillRect(i*10,j*10,10,10);
+            ctxBack.fillStyle = col;
+            ctxBack.fillRect(i*pixelSize,j*pixelSize,pixelSize,pixelSize);
         }
         if(k)
             k=false;
@@ -29,26 +42,20 @@ function drawGrid(){
     }
 }
 
-let mouseDown = 0;
-document.body.onmousedown = function() { 
-    mouseDown = 1;
-}
-document.body.onmouseup = function() {
-    mouseDown = 0;
-}
-
 function mouseClick(){
-    let x = event.clientX - Canvas.getBoundingClientRect().left;
-    let y = event.clientY - Canvas.getBoundingClientRect().top;
-    let p1 = convertPoint(x,y);
-    p2=p1;
-    drawLine(p2,p1);
-    
+    if(event.which == 1){    
+        let x = event.clientX - Canvas.getBoundingClientRect().left;
+        let y = event.clientY - Canvas.getBoundingClientRect().top;
+        
+        let p1 = convertPoint(x,y);
+        p2=p1;
+        drawLine(p2,p1);
+    }
 }
 
 function mouseMove(){
     
-    if(mouseDown==1){
+    if(event.which == 1){
         let x = event.clientX - Canvas.getBoundingClientRect().left;
         let y = event.clientY - Canvas.getBoundingClientRect().top;
         let p1 = convertPoint(x,y);
@@ -58,24 +65,31 @@ function mouseMove(){
 }
 
 function convertPoint(x,y){
-    return P(Math.floor(x/10)*10,Math.floor(y/10)*10);
+    return P(Math.floor(x/pixelSize)*pixelSize,Math.floor(y/pixelSize)*pixelSize);
 }
 function P(x,y){return {x:x,y:y}}
 
 function drawLine(P1,P2){
-    ctx.fillStyle = "black";
+    ctx.fillStyle = Color;
 
     let deltaX = Math.abs(P2.x - P1.x);
     let deltaY = Math.abs(P2.y - P1.y);
-    let signX = P1.x < P2.x ? 10 : -10;
-    let signY = P1.y < P2.y ? 10 : -10;
+    let signX = P1.x < P2.x ? pixelSize : -pixelSize;
+    let signY = P1.y < P2.y ? pixelSize : -pixelSize;
     
     let error = deltaX - deltaY;
     
-    ctx.fillRect(P2.x,P2.y,10,10);
+    if(toolIndex==1)
+        ctx.fillRect(P2.x,P2.y,pixelSize,pixelSize);
+    else
+        ctx.clearRect(P2.x, P2.y, pixelSize, pixelSize);
+
     while(P1.x != P2.x || P1.y != P2.y) 
    {        
-        ctx.fillRect(P1.x,P1.y,10,10);
+        if(toolIndex==1)
+            ctx.fillRect(P1.x,P1.y,pixelSize,pixelSize);
+        else
+            ctx.clearRect(P1.x, P1.y, pixelSize, pixelSize);
         let error2 = error * 2;
         
         if(error2 > -deltaY) 
